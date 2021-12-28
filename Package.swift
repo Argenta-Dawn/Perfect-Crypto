3aa0eb6f70133aad8cd8e941c4a0677cfc046b5c
wadecong <eva0612017@gmail.com>
2021-12-28 23:42:17 +0800

    update dependency Perfect

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
 		.library(name: "PerfectCrypto", targets: ["PerfectCrypto"])
 	],
     dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "4.0.0"),
+		.package(url: "https://github.com/Argenta-Dawn/Perfect.git", .branch("feature/glibc")),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
 		.package(url: cOpenSSLRepo, from: "4.0.0")
 	],
